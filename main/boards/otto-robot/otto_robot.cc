f1da8e62bbe8a718695c9d79a4bcfe5798a0ae52
niel987 <41085684+niel987@users.noreply.github.com>
2026-07-31 00:38:26 +0530

    otto_robot.cc

--- main/boards/otto-robot/otto_robot.cc
+++ main/boards/otto-robot/otto_robot.cc
@@ -175,36 +175,58 @@ private:
     }
 
     void InitializeLcdDisplay() {
-        esp_lcd_panel_io_handle_t panel_io = nullptr;
-        esp_lcd_panel_handle_t panel = nullptr;
-        esp_lcd_panel_io_spi_config_t io_config = {};
-        io_config.cs_gpio_num = hw_config_.display_cs_pin;
-        io_config.dc_gpio_num = hw_config_.display_dc_pin;
-        io_config.spi_mode = DISPLAY_SPI_MODE;
-        io_config.pclk_hz = 40 * 1000 * 1000;
-        io_config.trans_queue_depth = 10;
-        io_config.lcd_cmd_bits = 8;
-        io_config.lcd_param_bits = 8;
-        ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi(SPI3_HOST, &io_config, &panel_io));
+    esp_lcd_panel_io_handle_t panel_io = nullptr;
+    esp_lcd_panel_handle_t panel = nullptr;
 
-        esp_lcd_panel_dev_config_t panel_config = {};
-        panel_config.reset_gpio_num = hw_config_.display_rst_pin;
-        panel_config.rgb_ele_order = DISPLAY_RGB_ORDER;
-        panel_config.bits_per_pixel = 16;
+    esp_lcd_panel_io_spi_config_t io_config = {};
+    io_config.cs_gpio_num = hw_config_.display_cs_pin;
+    io_config.dc_gpio_num = hw_config_.display_dc_pin;
+    io_config.spi_mode = DISPLAY_SPI_MODE;
+    io_config.pclk_hz = 40 * 1000 * 1000;
+    io_config.trans_queue_depth = 10;
+    io_config.lcd_cmd_bits = 8;
+    io_config.lcd_param_bits = 8;
 
-        ESP_ERROR_CHECK(esp_lcd_new_panel_st7789(panel_io, &panel_config, &panel));
+    ESP_ERROR_CHECK(
+        esp_lcd_new_panel_io_spi(SPI3_HOST, &io_config, &panel_io)
+    );
 
-        esp_lcd_panel_reset(panel);
+    esp_lcd_panel_dev_config_t panel_config = {};
+    panel_config.reset_gpio_num = hw_config_.display_rst_pin;
+    panel_config.rgb_ele_order = DISPLAY_RGB_ORDER;
+    panel_config.bits_per_pixel = 16;
 
-        esp_lcd_panel_init(panel);
-        esp_lcd_panel_invert_color(panel, DISPLAY_INVERT_COLOR);
-        esp_lcd_panel_swap_xy(panel, DISPLAY_SWAP_XY);
-        esp_lcd_panel_mirror(panel, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y);
+    ESP_ERROR_CHECK(
+        esp_lcd_new_panel_st7789(panel_io, &panel_config, &panel)
+    );
 
-        display_ = new OttoEmojiDisplay(panel_io, panel, DISPLAY_WIDTH, DISPLAY_HEIGHT,
-                                        DISPLAY_OFFSET_X, DISPLAY_OFFSET_Y, DISPLAY_MIRROR_X,
-                                        DISPLAY_MIRROR_Y, DISPLAY_SWAP_XY);
-    }
+    esp_lcd_panel_reset(panel);
+    esp_lcd_panel_init(panel);
+
+    ESP_ERROR_CHECK(
+        esp_lcd_panel_set_gap(panel, 0, 20)
+    );
+
+    esp_lcd_panel_invert_color(panel, DISPLAY_INVERT_COLOR);
+    esp_lcd_panel_swap_xy(panel, DISPLAY_SWAP_XY);
+    esp_lcd_panel_mirror(
+        panel,
+        DISPLAY_MIRROR_X,
+        DISPLAY_MIRROR_Y
+    );
+
+    display_ = new OttoEmojiDisplay(
+        panel_io,
+        panel,
+        DISPLAY_WIDTH,
+        DISPLAY_HEIGHT,
+        DISPLAY_OFFSET_X,
+        DISPLAY_OFFSET_Y,
+        DISPLAY_MIRROR_X,
+        DISPLAY_MIRROR_Y,
+        DISPLAY_SWAP_XY
+    );
+}
 
     void InitializeButtons() {
         boot_button_.OnClick([this]() {
